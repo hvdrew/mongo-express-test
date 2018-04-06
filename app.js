23d9404119e36591b5d7e7487f4826a1eb44937e
Andrew <andrew.merison@gmail.com>
2018-04-06 20:59:38 +0000

    Added more content.

--- app.js
+++ app.js
@@ -5,4 +5,5 @@ const mongo = require('mongodb'),
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => res.render('pages/index.ejs'))
+app.get('/about', (req, res) => res.render('pages/about.ejs'))
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
