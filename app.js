9cf4ebb170a82e7daccb6503269ce8881a9af0b8
Andrew <andrew.merison@gmail.com>
2018-04-05 22:39:01 -0700

    Initial Commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const mongo = require('mongodb'),
+      express = require('express'),
+      app = express()
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => res.render('pages/index.ejs'))
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
